f50eac6d35d4c47ab306e21994e32f8ce3273589
swautomatetesting <sw.automate.testing@gmail.com>
2021-02-13 17:20:59 +0000

    First Java proj commit

---
+++ src/Sample1.java
@@ -0,0 +1,12 @@
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+        int grams=1750,milliGrams;
+		
+		milliGrams = grams*1000;
+		System.out.println("Grams to Milligrams conversion \n");
+		System.out.println(grams+" g "+" = "+milliGrams+" mg ");
+	}
+
+}
